a4be19ce75584448c7a0dd81bed15a4eebdfac3e
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-10-24 19:50:32 +0600

    Added spalash loading or creen loading are added

--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,16 +1,42 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Shared/Navbar/Navbar'
-import Footer from '../components/Shared/Footer/Footer'
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Shared/Navbar/Navbar";
+import Footer from "../components/Shared/Footer/Footer";
+import { useEffect, useState } from "react";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+
 const Main = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <div>
-      <Navbar />
-      <div className='pt-24 min-h-[calc(100vh-68px)]'>
-        <Outlet />
-      </div>
-      <Footer />
+      {loading ? (
+        <div className="flex justify-center items-center h-[100vh] w-[100%]">
+          <ClimbingBoxLoader
+            color={"#F37A24"}
+            loading={loading}
+            size={15}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <div className="pt-24 min-h-[calc(100vh-68px)]">
+            <l-hatch size="28" stroke="4" speed="3.5" color="black"></l-hatch>
+            <Outlet />
+          </div>
+          <Footer />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
