7095bc979991cf2ddc938194b5b1f34f67c7b0e6
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-11-09 21:53:04 +0600

    Image upload and show the image with upload button side

--- client/src/components/Shared/Navbar/Navbar.jsx
+++ client/src/components/Shared/Navbar/Navbar.jsx
@@ -29,9 +29,9 @@ const Navbar = () => {
               <div className="flex flex-row items-center gap-3">
                 {/* Become A Host btn */}
                 <div className="hidden md:block">
-                  {!user && (
+                  {user && (
                     <button
-                      disabled={!user}
+                      disabled={user}
                       className="disabled:cursor-not-allowed cursor-pointer hover:bg-neutral-100 py-3 px-4 text-sm font-semibold rounded-full  transition"
                     >
                       Host your home
